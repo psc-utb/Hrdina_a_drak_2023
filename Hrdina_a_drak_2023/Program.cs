be64d8546600d7d10c1d21609f1fc9a94a806782
psc-utb <psc.utb@gmail.com>
2023-02-20 15:05:01 +0100

    Zapisování do souboru, použití using na StreamWriter using použitý kvůli metodě Dispose, tzn. kvůli potřebě odstranění externích prostředků

--- Hrdina_a_drak_2023/Program.cs
+++ Hrdina_a_drak_2023/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Hrdina_a_drak_2023.Postavy;
 using Hrdina_a_drak_2023.Veci;
 using System;
+using System.IO;
 
 namespace Hrdina_a_drak_2023
 {
@@ -26,20 +27,33 @@ namespace Hrdina_a_drak_2023
 
 
             Mec mec = new Mec("Excalibur", 25);
+            mec = new Mec("obyčejný meč", 20);
             Hrdina hrdina = new Hrdina(100, mec);
             Drak drak = new Drak(120, 15);
 
-            while (hrdina.JeZiva() && drak.JeZiva())
+            using (StreamWriter streamWriter = File.CreateText("zaznam boje.txt"))
             {
-                double utokHrdiny = hrdina.Utok(drak);
-                Console.WriteLine($"Hrdina zaútočil hodnotou: {utokHrdiny}. Drak má {drak.Zdravi} zdraví." + Environment.NewLine);
-
-                if (drak.JeZiva())
+                while (hrdina.JeZiva() && drak.JeZiva())
                 {
-                    double utokDraka = drak.Utok(hrdina);
-                    Console.WriteLine($"Drak zaútočil hodnotou: {utokDraka}. Hrdina má {hrdina.Zdravi} zdraví.");
+                    double utokHrdiny = hrdina.Utok(drak);
+                    
+                    string textKvypisu = $"Hrdina zaútočil hodnotou: {utokHrdiny}. Drak má {drak.Zdravi} zdraví." + Environment.NewLine;
+                    Console.WriteLine(textKvypisu);
+                    streamWriter.WriteLine(textKvypisu);
+
+                    if (drak.JeZiva())
+                    {
+                        double utokDraka = drak.Utok(hrdina);
+
+                        textKvypisu = $"Drak zaútočil hodnotou: {utokDraka}. Hrdina má {hrdina.Zdravi} zdraví.";
+                        Console.WriteLine(textKvypisu);
+                        streamWriter.WriteLine(textKvypisu);
+                    }
+
+                    textKvypisu = Environment.NewLine + Environment.NewLine;
+                    Console.WriteLine(textKvypisu);
+                    streamWriter.WriteLine(textKvypisu);
                 }
-                Console.WriteLine(Environment.NewLine + Environment.NewLine);
             }
 
         }
